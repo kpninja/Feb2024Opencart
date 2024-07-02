27ff400b8bbf70315d8661a0bbf2bdae7342d0e6
KavitaPatil <kp.patil85@gmail.com>
2024-07-02 14:29:34 -0400

    updated demo page as per neetaTL comments

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -5,11 +5,15 @@ import org.openqa.selenium.By;
 public class DemoPage {
 
 	 By demo=  By.id("demo");
-	
+	 By cart=  By.id("cart");
 	
 	public void getDemo() {
 		System.out.println(demo);
+		System.out.println("demo details");
 	}
 	
-
+	public void addtoCart() {
+		System.out.println(cart);
+		System.out.println("cart details");
+	}
 }
